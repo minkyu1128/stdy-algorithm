fb093650662c562de7997a27a8ae047bbaa2898f
minkyu1128 <minkyu1128@gmail.com>
2023-04-07 10:37:32 +0900

    init

---
+++ src/main/java/com/mk/stdyalgorithm/StdyAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.mk.stdyalgorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdyAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StdyAlgorithmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mk/stdyalgorithm/StdyAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.stdyalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdyAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
